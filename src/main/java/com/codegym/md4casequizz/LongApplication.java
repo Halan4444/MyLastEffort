8bf4f24565ac8cefefbe4e4efbaca93e1f43e2ea
Trần Hạ Lan <halan4444b@gmail.com>
2022-07-18 23:28:09 +0700

    first commit

---
+++ src/main/java/com/codegym/md4casequizz/LongApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.md4casequizz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LongApplication.class, args);
+    }
+
+}
